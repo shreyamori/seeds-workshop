9a45b839a5574c6a48f39d6e84e2adab2e76a4f4
Simon <sstur@me.com>
2024-06-15 10:43:26 -0700

    Display post captions on home page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,27 @@
+import { useEffect, useState } from 'react';
 import { collection, getDocs } from 'firebase/firestore/lite';
-import { Button, YStack } from 'tamagui';
+import { Paragraph, YStack } from 'tamagui';
 
 import { db } from '../support/firebase';
 
-async function getPosts() {
-  const allPosts = await getDocs(collection(db, 'posts'));
-  return allPosts.docs.map((post) => ({ id: post.id, ...post.data() }));
-}
-
 export default function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const getPosts = async () => {
+      const postsRef = collection(db, 'posts');
+      const postsSnapshot = await getDocs(postsRef);
+      setPosts(postsSnapshot.docs);
+    };
+    void getPosts();
+  }, []);
+
   return (
     <YStack flex={1} justifyContent="center" alignItems="center">
-      <Button
-        onPress={async () => {
-          const posts = await getPosts();
-          for (const post of posts) {
-            console.log('>>', post);
-          }
-        }}
-      >
-        Get Posts
-      </Button>
+      <YStack gap={10}>
+        {posts.map((post) => (
+          <Paragraph>{post.data().caption}</Paragraph>
+        ))}
+      </YStack>
     </YStack>
   );
 }
